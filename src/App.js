a0f49c371a69f4d69be493083755ac398a45332b
Shiyu Wang <shiyu3169@gmail.com>
2019-06-24 11:35:58 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
 import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -29,6 +33,11 @@ function App() {
     <div className='container mt-5'>
       <h1 className='text-primary mb-3'>My blog</h1>
       <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
