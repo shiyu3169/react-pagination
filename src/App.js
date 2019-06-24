085333648953f24ad765ebbc10b662aabd4a35db
Shiyu Wang <shiyu3169@gmail.com>
2019-06-24 11:27:16 -0400

    initialize

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Axios from 'axios';
+import Posts from './components/Posts';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await Axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  return (
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+    </div>
+  );
+}
+
+export default App;
